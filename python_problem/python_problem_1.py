fe45d90916d26e3804492b17a33e9bab3e94848a
jaeyeong0901 <devlog.jae13@gmail.com>
2024-01-05 04:08:56 +0900

    1-6

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,32 +1,46 @@
 num = 0
 
-while True:
-  try:
-    user_num_input = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): '))
-    if user_num_input in [1, 2, 3]:
-      num = user_num_input
-      break
+while num <= 31:
+  while True:
+    try:
+      userA_num_input = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): '))
+      if userA_num_input in [1, 2, 3]:
+        break
+      else:
+        print('1, 2, 3 중 하나를 입력하세요')
+    except ValueError:
+      print('정수를 입력하세요')
+
+  for i in range(num + 1, num + userA_num_input + 1):
+    if i <= 31:
+      print(f'playerA : {i}')
+      i += 1
     else:
-      print('1, 2, 3 중 하나를 입력하세요')
-  except ValueError:
-    print('정수를 입력하세요')
-
-for i in range(1, num+1):
-  print(f'playerA : {i}')
-  i += 1
-
-
-
-while True:
-  try:
-    user_num_input = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): '))
-    if user_num_input in [1, 2, 3]:
       break
-    else:
-      print('1, 2, 3 중 하나를 입력하세요')
-  except ValueError:
-    print('정수를 입력하세요')
 
-for i in range(num+1, num + user_num_input + 1):
-  print(f'playerB : {i}')
-  i += 1
+  num += userA_num_input
+
+  if num >= 31:
+    break
+
+  while True:
+    try:
+      userB_num_input = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): '))
+      if userB_num_input in [1, 2, 3]:
+        break
+      else:
+        print('1, 2, 3 중 하나를 입력하세요')
+    except ValueError:
+      print('정수를 입력하세요')
+
+  for i in range(num+1, num + userB_num_input + 1):
+    if i <= 31:
+      print(f'playerA : {i}')
+      i += 1
+    else:
+      break
+      
+  num += userB_num_input
+
+  if num >= 31:
+    break
