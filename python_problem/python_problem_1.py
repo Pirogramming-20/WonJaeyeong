8dc90e3eb6aa4c62d9a2152bc09f1ac9fd224ed2
jaeyeong0901 <devlog.jae13@gmail.com>
2024-01-05 04:26:04 +0900

    1-9

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,30 +1,34 @@
+import random
+
 def brGame(num, player):
-    while True:
-        try:
-            num_input = int(input(f'{player}, 부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): '))
-            if num_input in [1, 2, 3]:
-                break
-            else:
-                print('1, 2, 3 중 하나를 입력하세요')
-        except ValueError:
-            print('정수를 입력하세요')
+    if player == "computer":
+        num_input = random.randint(1, 3)
+    else:
+        while True:
+            try:
+                num_input = int(input(f'부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
+                if num_input in [1, 2, 3]:
+                    break
+                else:
+                    print('1, 2, 3 중 하나를 입력하세요')
+            except ValueError:
+                print('정수를 입력하세요')
 
     for i in range(num + 1, num + num_input + 1):
         if i > 31:
             return 31
-        print(f'{player} : {i}')
+        print(f'{player} {i}')
 
     return num + num_input
 
 num = 0
-
-while num < 31:
-    num = brGame(num, 'playerA')
-    if num >= 31:
-        print('playerB win!')
-        break
-
-    num = brGame(num, 'playerB')
-    if num >= 31:
-        print('playerA win!')
-        break
+while num < 31:  
+  num = brGame(num, 'computer')
+  if num >= 31:
+      print('player wins!')
+      break
+  
+  num = brGame(num, 'player')
+  if num >= 31:
+    print('computer wins!')
+    break
