51dad45e1c6b41535cb77e4e67c8ff148e5513fe
jaeyeong0901 <devlog.jae13@gmail.com>
2024-01-05 03:16:25 +0900

    1-4

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -4,8 +4,12 @@ while True:
   try:
     user_num_amount = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): '))
     if user_num_amount in [1, 2, 3]:
-      print(user_num_amount)
+      break
     else:
       print('1, 2, 3 중 하나를 입력하세요')
   except ValueError:
     print('정수를 입력하세요')
+
+for i in range(1, user_num_amount + 1):
+  print(f'player : {i}')
+  i += 1
