848ac9a5c114c47f6532c9bc1d365f9bf5fc7352
jaeyeong0901 <devlog.jae13@gmail.com>
2024-01-05 02:59:29 +0900

    1-3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,4 +1,12 @@
 num = 0
 
-num_amount = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-
+def get_num_amount():
+  while True:
+    try:
+      num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): '))
+      if num in [1, 2, 3]:
+        return num
+      else:
+        print('1, 2, 3 중 하나를 입력하세요')
+    except ValueError:
+      print('정수를 입력하세요')
